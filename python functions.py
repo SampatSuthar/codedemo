9f31f514032502f24d54f5e03491023b7f7d682d
SampatSuthar <45846023+SampatSuthar@users.noreply.github.com>
2023-06-20 18:41:34 +0530

    Create python functions.py

---
+++ python functions.py
@@ -0,0 +1,16 @@
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+def abstracttive_summary(meeting_note):
+    # Load tokenizer 
+    tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-xsum")
+    # Load model 
+    model = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
+    # Create tokens - number representation of our text
+    tokens = tokenizer(text, truncation=True, padding="longest", return_tensors="pt")
+    #tokens
+    # Summarize 
+    summary = model.generate(**tokens)
+    # Output summary tokens         
+    #summary[0]
+    # Decode summary
+    output = tokenizer.decode(summary[0])
+    return output
